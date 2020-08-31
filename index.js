b4f8fe4dcbec610abd5df24c76bf77eab8833d65
jonny <soto.jonathan6@gmail.com>
2020-08-31 12:48:56 -0400

    added api calls, styling, and created gitignore file

--- index.js
+++ index.js
@@ -6,30 +6,78 @@
 
 const topic = document.getElementById("new-topic");
 const description = document.getElementById("description-area");
+const video_area = document.getElementsByClassName("video-results-1")[0];
+const HQ_KEY = config.HQ_KEY;
+const YT_KEY = config.YT_KEY;
 
 
 
-var config = {
+var HQ_config = {
   method: 'get',
   url: 'https://api.predicthq.com/v1/events/?local_rank\n',
   headers: { 
-    'Authorization': 'Bearer access'
+    'Authorization': 'Bearer ' + HQ_KEY
   }
 };
 
 
-// Using config to get the data and change the topic <p> 
-axios(config)
-.then(function (response) {
-    JSON.stringify(response.data)
 
+// Using config to get the data and change the topic <p> 
+axios(HQ_config)
+.then(function (response) {
 
     topic.innerHTML = response.data.results[1].title;
     description.innerHTML = response.data.results[1].description
 
 
-    console.log(JSON.stringify(response.data));
+// request based on the event returned by the PredictHQ API 
+    let one = 'https://www.googleapis.com/youtube/v3/search?key='+ YT_KEY +'&part=snippet&q='+ response.data.results[1].title
+    let two = "https://www.googleapis.com/youtube/v3/search?key=" + YT_KEY + "&part=snippet&q=dogs"
+    
+    
+    const requestOne = axios.get(one);
+    
+    const requestTwo = axios.get(two);
+    
+    //makes request to the google/youtube search api
+    axios.all([requestOne, requestTwo]).then(axios.spread((...responses) => {
+      const responseOne = responses[0]
+      const responseTwo = responses[1]
+    
+
+      const url = JSON.parse(JSON.stringify(responseOne.data.items[1].snippet.thumbnails.high.url))
+
+        video_area.setAttribute("src", url)
+        video_area.src = JSON.stringify(responseOne.data.items[1].snippet.thumbnails.high.url);
+    
+
+//creating an iFrame for the youtube video
+
+        for(var i = 0; i<3; i++){
+
+            var ifrm = `
+        <iframe src="http://www.youtube.com/embed/${JSON.parse(JSON.stringify(responseOne.data.items[i].id.videoId))} frameborder='0' allowfullscreen"></iframe>
+        
+        `
+        document.getElementsByClassName("video-results-" + (i+1).toString())[0].innerHTML = ifrm;
+        }
+        
+        
+    //     document.createElement("iframe");
+    //    ifrm.setAttribute("src", "http://www.youtube.com/embed/" + JSON.stringify(responseOne.data.items[1].id.videoId));
+    //    video_area.appendChild(ifrm);
+
+
+      //use/access the results 
+    })).catch(errors => {
+      console.log(errors);
+    })
+
+   //console.log(JSON.stringify(response.data));
 })
 .catch(function (error) {
   console.log(error);
 });
+
+
+
